384c2b38ff5d088fc0fe305b54586070017d4829
Gabriel G. R. Silva <gabrielgrsilva@outlook.com.br>
2025-10-24 14:17:32 -0300

    Add get/listen logic

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,15 @@ import express from "express";
 
 const app = express();
 
-app.listen(8080)
+const PORT = 8080;
+
+app.use("/app", express.static("./src/app"));
+
+app.get("/healthz", (req, res) => {
+    res.set("Content-Type", "text/plain; charset=utf-8");
+    res.send("OK");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
