98603e5a0f0e7c20fed35354c856b112071f6255
Gabriel G. R. Silva <gabrielgrsilva@outlook.com.br>
2025-10-24 14:47:49 -0300

    Add status checking middleware

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,24 @@ const PORT = 8080;
 
 app.use("/app", express.static("./src/app"));
 
+app.use((req, res, next) => {
+  res.on("finish", () => { //Listens for finish events to check their status and log if failed
+  const status = res.statusCode
+
+  if (status < 200 || status > 299){
+    console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${status}`);
+      };
+    });
+    next();
+});
+
 app.get("/healthz", (req, res) => {
     res.set("Content-Type", "text/plain; charset=utf-8");
     res.send("OK");
+
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
+
