0ed86b43e08ea955d6d43f3f9d85f30e498ee008
Gabriel G. R. Silva <gabrielgrsilva@outlook.com.br>
2025-10-29 08:21:00 -0300

    Refactor to switch, from ifelse, errorHandler

--- src/errormiddleware.ts
+++ src/errormiddleware.ts
@@ -1,5 +1,5 @@
-import { Request, Response, NextFunction, ErrorRequestHandler } from 'express';
-import * as ec from "./errorclasses.js"
+import { Request, Response, NextFunction, ErrorRequestHandler } from "express";
+import * as ec from "./errorclasses.js";
 
 export const errorHandler: ErrorRequestHandler = (
   err: Error,
@@ -7,18 +7,19 @@ export const errorHandler: ErrorRequestHandler = (
   res: Response,
   next: NextFunction,
 ) => {
-  console.error(err); //Logs error in console and returns JSON res below
-  if (res.headersSent) return next(err); //Guard in case res was already sent
+  if (res.headersSent) return next(err);
 
-  if(err instanceof ec.BadRequest){
-    res.status(400).json({error: err.message});
-  }else if(err instanceof ec.Unauthorized){
-    res.status(401).json({error: err.message});
-  }else if(err instanceof ec.Forbidden){
-    res.status(403).json({error: err.message});
-  }else if(err instanceof ec.NotFound){
-    res.status(404).json({error: err.message});
-  }else{
-  res.status(500).json({error: "Something went wrong on our end"});
-  };
+  switch (true as const) {
+    case err instanceof ec.BadRequest:
+      return res.status(400).json({ error: err.message });
+    case err instanceof ec.Unauthorized:
+      return res.status(401).json({ error: err.message });
+    case err instanceof ec.Forbidden:
+      return res.status(403).json({ error: err.message });
+    case err instanceof ec.NotFound:
+      return res.status(404).json({ error: err.message });
+    default:
+      console.error(err); // log unexpected errors
+      return res.status(500).json({ error: "Internal Server Error" });
+  }
 };
